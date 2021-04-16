4072d2167a0b3b3fbc11e9884aa2f02c6344b625
Artem Pisarenko <artem.k.pisarenko@gmail.com>
2021-04-17 00:18:21 +0600

    next release

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -60,7 +60,7 @@
 #include <pgmspace.h>
 
 // increment on change
-#define SOFTWARE_VERSION_STR "NRZ-2020-134-B1-airomsk3"
+#define SOFTWARE_VERSION_STR "NRZ-2020-134-B1-airomsk4"
 String SOFTWARE_VERSION(SOFTWARE_VERSION_STR);
 
 /*****************************************************************
